2ca77816b271915a766e39c62107ccc1405cfcc0
Dennis Wentzien <49585347+wentzien@users.noreply.github.com>
2020-10-14 10:10:44 +0200

    serve app from subdirectory

---
+++ build/precache-manifest.f9f2fd239486b633b198f9e8c888af7c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6824f56f681b32029a79d60a2ee3d8f4",
+    "url": "/questions/index.html"
+  },
+  {
+    "revision": "64cd68df8ee48bd6550b",
+    "url": "/questions/static/css/2.9f71e5cf.chunk.css"
+  },
+  {
+    "revision": "f3cf4a8cf8a2ead4609e",
+    "url": "/questions/static/css/main.38bbf11a.chunk.css"
+  },
+  {
+    "revision": "64cd68df8ee48bd6550b",
+    "url": "/questions/static/js/2.b288cc13.chunk.js"
+  },
+  {
+    "revision": "e7100ecebec993ee4364118490022cf2",
+    "url": "/questions/static/js/2.b288cc13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3cf4a8cf8a2ead4609e",
+    "url": "/questions/static/js/main.7344c4ab.chunk.js"
+  },
+  {
+    "revision": "7847c443749eae9783fb",
+    "url": "/questions/static/js/runtime-main.b196187b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/questions/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/questions/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/questions/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/questions/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/questions/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
